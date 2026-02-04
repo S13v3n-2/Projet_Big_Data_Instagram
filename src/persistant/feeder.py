796321bce27e06fda971d76161377b50b4c6d080
Carlot Steven <steven.carlotpro@gmail.com>
2026-02-04 23:40:38 +0100

    importation CSV dans le raw

--- src/persistant/feeder.py
+++ src/persistant/feeder.py
@@ -1,6 +1,7 @@
 from datetime import date
 from pyspark.sql import SparkSession, functions as F
 from pyspark.sql.types import *
+import sqlite3
 import time
 
 spark = (
@@ -14,20 +15,38 @@ spark = (
 print("Demarrage ingestion Bronze")
 
 # Ingestion CSV Profils
-input_csv_path = "file:///source/instagram_users_lifestyle.csv"
+input_csv_path = "file:///source/instagram.csv"
 
 schema_profiles = StructType([
-    StructField("user_id", IntegerType(), False),
-    StructField("age", IntegerType(), True),
+    StructField("app_name", StringType(), True),
     StructField("gender", StringType(), True),
-    StructField("country", StringType(), True),
-    StructField("content_type_preference", StringType(), True),
+    StructField("urban_rural", StringType(), True),
+    StructField("employment_status", StringType(), True),
+    StructField("relationship_status", StringType(), True),
+    StructField("exercise_hours_per_week", DoubleType(), True),
+    StructField("diet_quality", StringType(), True),
+    StructField("alcohol_frequency", StringType(), True),
+    StructField("self_reported_happiness", IntegerType(), True),
+    StructField("blood_pressure_systolic", IntegerType(), True),
+    StructField("daily_steps_count", IntegerType(), True),
+    StructField("hobbies_count", IntegerType(), True),
+    StructField("books_read_per_year", IntegerType(), True),
+    StructField("travel_frequency_per_year", IntegerType(), True),
+    StructField("sessions_per_day", IntegerType(), True),
+    StructField("reels_watched_per_day", IntegerType(), True),
+    StructField("likes_given_per_day", IntegerType(), True),
+    StructField("dms_sent_per_week", IntegerType(), True),
+    StructField("ads_viewed_per_day", IntegerType(), True),
+    StructField("time_on_feed_per_day", IntegerType(), True),
+    StructField("time_on_messages_per_day", IntegerType(), True),
+    StructField("followers_count", IntegerType(), True),
+    StructField("uses_premium_features", StringType(), True),
+    StructField("account_creation_year", IntegerType(), True),
+    StructField("average_session_length_minutes", DoubleType(), True),
     StructField("preferred_content_theme", StringType(), True),
-    StructField("perceived_stress_score", IntegerType(), True),
-    StructField("weekly_work_hours", IntegerType(), True),
-    StructField("exercise_hours_per_week", IntegerType(), True),
-    StructField("income_level", StringType(), True),
-    StructField("education_level", StringType(), True)
+    StructField("two_factor_auth_enabled", StringType(), True),
+    StructField("linked_accounts_count", IntegerType(), True),
+    StructField("user_engagement_score", DoubleType(), True)
 ])
 
 df_profiles = (
@@ -53,14 +72,13 @@ df_profiles_bronze.cache()
 
 output_profiles = "hdfs://namenode:9000/lakehouse/bronze/instagram_users_profiles"
 
-time.sleep(60)
-
+#time.sleep(60)
 (
     df_profiles_bronze
     .repartition(8)
     .write
     .mode("overwrite")
-    .partitionBy("year", "month", "day", "country")
+    .partitionBy("year", "month", "day", "urban_rural")
     .parquet(output_profiles)
 )
 
